35a99abe569b0dbeec70e07c4257437711058486
Jason Lacy <jdlacy@gmail.com>
2023-02-19 18:46:50 -0800

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jason on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
